940465946a4883236eaf1a5753ab1f967f0a0334
sdvc616 <abdullah.sdvc@gmail.com>
2026-05-26 18:01:31 +0500

    fixed backend frontend structure

--- backend/server.js
+++ backend/server.js
@@ -80,6 +80,6 @@ app.post("/login", (req, res) => {
 // SERVER START
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("🚀 Server running on port " + PORT);
 });
